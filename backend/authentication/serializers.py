ea6efa544e75f53f0776ccfdac6657988e7fa8f4
turhvjbufv <91851160+Turhvjbufv@users.noreply.github.com>
2025-11-28 12:53:34 +0200

    Backend: added strong password cheking + better error messages

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
-
+import math
+import re
 
 class UserCreateSerializer(serializers.ModelSerializer):
     """Serializer for user signup/registration"""
@@ -10,8 +11,53 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name']
         extra_kwargs = {
-            'password': {'write_only': True},
+            'password': {
+                'write_only': True,
+                'error_messages': {
+                    'max_length': 'Password cannot have more than 255 characters.'
+                }
+            },
+            'username': {
+                'error_messages': {
+                    'max_length': 'Username cannot have more than 100 characters.'
+                }
+            }
         }
+    
+    def validate_username(self, value):
+        """Validate username length"""
+        if len(value) > 100:
+            raise serializers.ValidationError("Username cannot have more than 100 characters.")
+        return value
+
+    def validate_password(self, value):
+        """
+        Check that the password is strong, I know that it is shitty checking, but 
+        Fuck it
+        """
+        if len(value) > 255:
+            raise serializers.ValidationError("Password cannot have more than 255 characters.")
+        def calculate_entropy(password: str) -> float:
+            """Approximate KeePassXC-style entropy in bits."""
+            charset = 0
+
+            if re.search(r'[a-z]', password):
+                charset += 26
+            if re.search(r'[A-Z]', password):
+                charset += 26
+            if re.search(r'[0-9]', password):
+                charset += 10
+            if re.search(r'[!@#$%^&*(),.?":{}|<>\[\]\\\/;\'`~_\-+=]', password):
+                charset += 33
+
+            if charset == 0:
+                return 0
+            return len(password) * math.log2(charset)
+
+        entropy = calculate_entropy(value)
+        if entropy < 60: # TODO: up it to 80 for production
+            raise serializers.ValidationError("Password must be stronger, challenge yourself.")
+        return value
     
     def validate(self, data):
         """Validate that passwords match"""

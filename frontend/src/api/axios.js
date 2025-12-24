edfff5897e2d923ddeded5d7bf7d986ae053a4cd
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-12-24 10:16:23 +0200

    create account has been set to use hash function

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8001/api',
+    baseURL: `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:8001/api',
     headers: {
         'Content-Type': 'application/json',
     },
